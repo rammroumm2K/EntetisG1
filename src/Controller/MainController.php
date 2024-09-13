aa4378b9a83a3d7589824c1edcf0d1285a23f2a9
rammroumm2K <ramybrz00@gmail.com>
2024-09-13 09:15:10 +0200

    v1.0

---
+++ src/Controller/MainController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController extends AbstractController
+{
+    #[Route('/', name: 'homepage')]
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig',[
+           // 'controller_name' => 'MainController',
+            'title' => 'Homepage',
+            'homepage_text' => "Nous somme le ".date('d/m/Y \à H:i'),
+
+        ]);
+    }
+    #[Route('/about', name: 'about_me')]
+    public function aboutMe(): Response
+    {
+        return $this->render('main/about.html.twig',[
+            'title' => 'About me',
+            'homepage_text' => "Good Afternoon my dear",
+        ]);
+    }
+}
